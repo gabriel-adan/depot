834812b5d0d6b34dd3e73e2bf58dd8f69202caf8
gabriel-adan <gabrieladansantillan@hotmail.com>
2016-03-01 11:19:45 -0300

    Validacion de los campos del modelo Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	validates :title, uniqueness: true
+	validates :price, numericality: {greater_than_equal_to: 0.01}
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(jpg|png|gif)\Z}i,
+		message: 'Debe ser una Url de imagen con terminacion JPG, PNG O GIF.'
+	}
 end
