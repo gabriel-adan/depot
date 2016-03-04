511654223785340044308a323c0a1cbb8a6afe4a
gabriel-adan <gabrieladansantillan@hotmail.com>
2016-03-04 11:23:56 -0300

    Integracion con JQueryUI, Agregado de evento click a las imagenes de lineas de items para agregar productos al Cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def hidden_div_if(condition, attribures = {}, &block)
+		if condition
+			attribures["style"] = "display: none"
+		end
+		content_tag("div", attribures, &block)
+	end
 end
